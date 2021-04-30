885b33d0652de06aef5839e6a1499c4579db1996
corentinperroux <corentin7301@gmail.com>
2021-04-30 16:55:50 +0100

    images optimization

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import 'alpinejs'
-import './animation'
-import '../assets/tailwind.css'
-import '../assets/tailwindPerso.css'
+import 'alpinejs';
+import './animation';
+import '../assets/tailwind.css';
+import '../assets/tailwindPerso.css';
